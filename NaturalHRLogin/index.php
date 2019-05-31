1a8ddc468e2b91bee36cdb82ad61ebb8d7c6811d
Will <triplenutwh@gamil.com>
2019-05-31 11:36:43 +0100

    Removed option to reset password.

--- NaturalHRLogin/index.php
+++ NaturalHRLogin/index.php
@@ -21,8 +21,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["firstname"]); ?></b>. Welcome to our site.</h1>
     </div>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+    <p>        
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
 </body>
